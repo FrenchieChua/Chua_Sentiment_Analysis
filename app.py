0345644a755e0bdd6e4045bb3eddc43f0c21fe62
FrenchieChua <114549922+FrenchieChua@users.noreply.github.com>
2023-05-05 12:49:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -210,8 +210,7 @@ def app():
         plt.pie(sizes, labels = labels, textprops={'fontsize': 10}, startangle=140, \
                 autopct='%1.0f%%', colors=custom_colours, explode=[0, 0.05])
         plt.subplot(1, 2, 2)
-        sns.barplot(x = result['Sentiment'].unique(), y = result['Sentiment'].value_counts(), \
-                palette= 'viridis')
+        sns.barplot(x = ['Negative', 'Positive'], y = counts, palette= 'viridis')
         st.pyplot(fig)
 
         # Save the dataframe to a CSV file
